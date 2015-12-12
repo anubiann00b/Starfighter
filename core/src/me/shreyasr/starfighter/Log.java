aaa626f32ff2bb65f85156fecb6af3b3d1f008e8
Shreyas <skraman1999@gmail.com>
2015-12-11 22:49:40 -0800

    Confirmed working on web and desktop.

--- core/src/me/shreyasr/starfighter/Log.java
+++ core/src/me/shreyasr/starfighter/Log.java
@@ -1,18 +1,50 @@
 package me.shreyasr.starfighter;
 
+import com.badlogic.gdx.Application;
+import com.badlogic.gdx.Gdx;
+
+import java.util.logging.Handler;
 import java.util.logging.Level;
+import java.util.logging.LogRecord;
 import java.util.logging.Logger;
 
 public class Log {
 
-    private static Logger logger = Logger.getLogger("");
+    private static Logger logger = Logger.getLogger("Main Logger");
+    static {
+        if (Gdx.app.getType() != Application.ApplicationType.WebGL) {
+            setConsoleHandler(logger);
+        }
+    }
 
     public static void e(String msg) {
         logger.log(Level.SEVERE, msg);
     }
 
     public static void i(String msg) {
+        e(Gdx.app.getType().name());
         logger.log(Level.INFO, msg);
+    }
+
+    private static void setConsoleHandler(Logger logger) {
+        logger.setUseParentHandlers(false);
+        logger.addHandler(new Handler() {
+            @Override
+            public void publish(LogRecord record) {
+                System.out.println(record.getLevel().getName() + ": "
+                        + record.getMessage());
+            }
+
+            @Override
+            public void flush() {
+                System.out.flush();
+            }
+
+            @Override
+            public void close() throws SecurityException {
+
+            }
+        });
     }
 }
 
